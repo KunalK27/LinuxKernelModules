c7ca84471f23fe8790a9a201d81f3f02085f479d
Kunal Kulkarni <56317013+KunalK27@users.noreply.github.com>
2021-04-05 21:50:49 +0530

    Updates for Article (#1)
    * Update CharDeviceDriver.c
    
    * Update Implementation2.c
    
    * Update Makefile

--- CharDeviceDriver.c
+++ CharDeviceDriver.c
@@ -2,7 +2,6 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>	
-
 int init_module(void);
 void cleanup_module(void);
 static int device_open(struct inode *, struct file *);
--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
 obj−m += hello.o 
-
 all: make −C /lib/modules/$(shell uname −r)/build M=$(PWD) modules 
-
 clean: make −C /lib/modules/$(shell uname −r)/build M=$(PWD) clean
